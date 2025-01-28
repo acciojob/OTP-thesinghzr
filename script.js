c0893ee03e382bc3b3594b517883bcbb2c7cda1b
Suraj Singh <yousurajsingh@gmail.com>
2025-01-29 01:35:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+function ok() {
+    const inputs = document.querySelectorAll('.code');
+    
+    inputs[0].focus();
+
+    inputs.forEach((input, index) => {
+        input.addEventListener('input', (event) => {
+            if (event.target.value.length === 1 && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        });
+
+        input.addEventListener('keydown', (event) => {
+            if (event.key === 'Backspace' && event.target.value === '') {
+                if (index > 0) {
+                    inputs[index - 1].focus();
+                }
+            }
+        });
+    });
+}
+ok();
